280d6e1b06a891b50a30389b642c4ca3d788578b
Tom Allen <tswallen@outlook.com>
2018-10-21 23:36:40 +1100

    Added router links to property

--- src/app/properties/property/property.component.ts
+++ src/app/properties/property/property.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Property } from '../property';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { PropertyService } from 'src/app/property.service';
 
 @Component({
   selector: 'app-property',
@@ -9,9 +12,24 @@ import { Property } from '../property';
 export class PropertyComponent implements OnInit {
   @Input() property: Property;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private propertyService: PropertyService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getProperty();
+  }
+
+  getProperty(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.propertyService.getProperty(id)
+      .subscribe(property => this.property = property);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
